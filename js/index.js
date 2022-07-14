7baa211e043574dba4310dfdea216829a31a6a8e
simonbarrau <simonbarrau@gmail.com>
2022-07-14 20:45:55 -0300

    JavaSccript

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-alert("Wear It")
 
 
 class Ropa{
@@ -27,6 +26,13 @@ const buzo3 = new Ropa("Orange 666", 10000, "envio gratis","Descuento:OFF", "sto
 const buzo4 = new Ropa("Green Teeth", 10000, "envio gratis","Descuento:ON", "stock: Available")
 const buzo5 = new Ropa("Trap Watermelon", 10000, "envio gratis","Descuento:OFF", "stock: Out of")
 const buzo6 = new Ropa("Spine 22", 10000, "envio gratis","Descuento:ON", "stock: Available")
+const remera1= new Ropa("Classic White",5000 ,"envio gratis","Descuento:ON", "stock: Available")
+const remera2= new Ropa("Orange large Design",7000 ,"envio gratis","Descuento:OFF", "stock: Available")
+const remera3= new Ropa("Black Large 7",7000 ,"envio gratis","Descuento:ON", "stock: Available")
+const remera4= new Ropa("Large Red Logo",7000 ,"envio gratis","Descuento:OFF", "stock: Available")
+const remera5= new Ropa("Classic Black & White Design",5000 ,"envio gratis","Descuento:ON", "stock: Available")
+const remera6= new Ropa("Large Cool",7000 ,"envio gratis","Descuento:OFF", "stock: Available")
+
 let prendas= [];
 prendas.push(buzo1);
 prendas.push(buzo2);
@@ -34,17 +40,29 @@ prendas.push(buzo3);
 prendas.push(buzo4);
 prendas.push(buzo5);
 prendas.push(buzo6);
+prendas.push(remera1);
+prendas.push(remera2);
+prendas.push(remera3);
+prendas.push(remera4);
+prendas.push(remera5);
+prendas.push(remera6);
 
 buzo1.calcIva ();
 buzo3.calcIva ();
 buzo5.calcIva ();
+remera2.calcIva();
+remera4.calcIva();
+remera6.calcIva();
 
 buzo2.calcDescuento ();
 buzo4.calcDescuento ();
 buzo6.calcDescuento();
+remera1.calcDescuento();
+remera3.calcDescuento();
+remera5.calcDescuento();
 
-
-
+prendas.reverse();
+let encontrar=prendas.filter(item => item.descuento === "Descuento:ON");
 
 for(const item of prendas){
 if(item.nombre === "Trap Watermelon"){
@@ -54,5 +72,5 @@ if(item.nombre === "Orange 666"){
     item.vender()
 }
 }
-prendas.forEach(elemento=>console.log(elemento));
 
+prendas.forEach(elemento=>console.log(elemento));
